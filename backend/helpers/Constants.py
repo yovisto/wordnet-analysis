59ce0e413af29c8bf0e5f65e51f1a1630dd74903
pssvlng <info@passivlingo.com>
2023-07-22 20:42:23 +0200

    file fix

--- backend/helpers/Constants.py
+++ backend/helpers/Constants.py
@@ -1,3 +1,4 @@
 VALID_WORDNET_LANGS = ['eng', 'fra', 'spa', 'ita', 'nld', 'por', 'fas', 'jpn', 'tha', 'pol']
 VALID_EU_LANGS = ['eng', 'fra', 'spa', 'ita', 'nld', 'por', 'ger']
 DOMAIN_NAMES = ['.co', '.de', '.nl', '.pt', '.es', '.org', '.net', '.fr', '.it', '.net', '.com', '.edu', '.gov']
+
